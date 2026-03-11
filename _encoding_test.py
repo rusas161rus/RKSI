7e552e6b6d19f68cb5a05d78ef06f5036e08690b
rusas161rus <rusas161rus@users.noreply.github.com>
2026-03-11 15:40:46 +0300

    Добавить графики CPU и памяти

---
+++ _encoding_test.py
@@ -0,0 +1,6 @@
+﻿from docx import Document
+
+d = Document()
+d.add_paragraph('Проверка кириллицы: Дипломный проект, Расписание занятий, РКСИ')
+d.save(r'E:\codex_progects\RKSI_ubuntu\_encoding_test.docx')
+print('ok')
